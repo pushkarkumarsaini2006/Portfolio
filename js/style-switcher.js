76f72c659422f9e8265bb30974a049b07d572c5f
Pushkar Kumar Saini <pushkarkumarsaini2006@gmail.com>
2025-02-18 21:29:48 +0530

    fifth commit

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -24,15 +24,15 @@ function setActiveStyle(color) {
 /* ========================== theme light and dark mode =========================== */
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
     document.body.classList.toggle("light");
 })
 window.addEventListener("load", () => {
     if (document.body.classList.contains("light")) {
-        dayNight.querySelector("i").classList.add("fa-sun");
+        dayNight.querySelector("i").classList.add("fa-moon");
     }
     else {
-        dayNight.querySelector("i").classList.add("fa-moon");
+        dayNight.querySelector("i").classList.add("fa-sun");
     }
 })
